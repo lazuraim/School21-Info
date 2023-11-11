a97c7a475c0d931a5631a4635a22b1f17018dcae
Mamie Malwyn <mamiemal@student.21-school.ru>
2023-11-11 16:03:04 +0300

    edit

--- src/test.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    menu.pizza_name AS pizza_name,
-    menu.price,
-    pizzeria.name AS pizzeria_name,
-    person_visits.visit_date
-FROM person_visits
-INNER JOIN menu ON person_visits.pizzeria_id = menu.pizzeria_id
